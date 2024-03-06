7e98cdf3edd8dd478ba8069a8f0aa8b470655ffa
Nuaiman Hasan <142655538+nothing015@users.noreply.github.com>
2024-03-06 15:50:15 -0500

    Update background.py

--- objects/background.py
+++ objects/background.py
@@ -1,18 +1,21 @@
-import pygame.sprite
-
-import assets
 import configs
+from layer import Layer
 
 
 # All about the background of the game and updating it constantly
 class Background(pygame.sprite.Sprite):
     def __init__(self, index, *groups):
-        # Initialize the sprite
-        super().__init__(*groups)
+
+        # Rendering order of sprites, collision detection, and other game logic that depends on the layering of objects in the game scene
+        self._layer = Layer.BACKGROUND
         # Load the background image sprite
-        """   29:44   """
+
         self.image = assets.get_sprite("background")
         self.rect = self.image.get_rect(topleft=(configs.SCREEN_WIDTH*index,0))
+        # Initialize the sprite
+        super().__init__(*groups)
+
+
 
     def update(self):
         # Move the background towards the left
