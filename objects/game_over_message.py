d7757805f23b5f725ea308291f4fc7f39cae9151
Nuaiman Hasan <142655538+nothing015@users.noreply.github.com>
2024-05-14 06:49:48 +0600

    Update game_over_message.py

---
+++ objects/game_over_message.py
@@ -0,0 +1,19 @@
+import pygame.sprite
+
+import assets
+import configs
+from layer import Layer
+
+
+class GameOverMessage(pygame.sprite.Sprite):
+    # Rendering order of sprites, collision detection, and other game logic that depends on the layering of objects
+    # in the game scene
+    def __init__(self, *groups):
+        self._layer = Layer.UI
+        # Load the floor image sprite
+        self.image = assets.get_sprite("gameover")
+        self.rect = self.image.get_rect(center=(configs.SCREEN_WIDTH/2, configs.SCREEN_HEIGHT/2))
+        self.mask = pygame.mask.from_surface(self.image)
+
+        # Initialize the sprite
+        super().__init__(*groups)
